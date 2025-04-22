dad7736bc761b960627030fe98b638741411eaaa
Adam Boczek <adam.boczek@nttdata.com>
2025-04-22 18:17:19 +0200

    feat: add initial CRD specification

--- api/v1alpha1/awsmskdemokafkaacl_types.go
+++ api/v1alpha1/awsmskdemokafkaacl_types.go
@@ -20,22 +20,16 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // AwsMSKDemoKafkaACLSpec defines the desired state of AwsMSKDemoKafkaACL
 type AwsMSKDemoKafkaACLSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of AwsMSKDemoKafkaACL. Edit awsmskdemokafkaacl_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	TopicName string `json:"topicName"`
+	Principal string `json:"principal"`
+	Operation string `json:"operation"`
 }
 
 // AwsMSKDemoKafkaACLStatus defines the observed state of AwsMSKDemoKafkaACL
 type AwsMSKDemoKafkaACLStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Status string `json:"status"`
 }
 
 // +kubebuilder:object:root=true
--- api/v1alpha1/awsmskdemokafkatopic_types.go
+++ api/v1alpha1/awsmskdemokafkatopic_types.go
@@ -20,22 +20,16 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // AwsMSKDemoKafkaTopicSpec defines the desired state of AwsMSKDemoKafkaTopic
 type AwsMSKDemoKafkaTopicSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of AwsMSKDemoKafkaTopic. Edit awsmskdemokafkatopic_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Name              string `json:"name"`
+	Partitions        int32  `json:"partitions"`
+	ReplicationFactor int32  `json:"replicationFactor"`
 }
 
 // AwsMSKDemoKafkaTopicStatus defines the observed state of AwsMSKDemoKafkaTopic
 type AwsMSKDemoKafkaTopicStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Status string `json:"status,omitempty"`
 }
 
 // +kubebuilder:object:root=true
